f2855c2a70fa7b0172a95554686e857cd5461ef2
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-15 16:54:24 -0300

    refac: changing front end files path

--- web/src/components/Movies.tsx
+++ web/src/components/Movies.tsx
@@ -5,12 +5,9 @@ const Movies = () => {
     <div className="grid grid-cols-3 gap-8 w-[85%] mt-12 mx-auto">
       {movies.map((movie) => (
         <div className="h-[45vh] w-full " key={movie.title}>
-          <img
-            src={movie.poster}
-            alt={movie.title}
-            className="h-full w-full object-cover rounded"
-          />
-          {/* <div className={`h-full w-full bg-[url(${movie.poster}]`}></div> */}
+          <div
+            className={`h-full w-full bg-[url('${movie.poster}')] bg-no-repeat bg-cover`}
+          ></div>
         </div>
       ))}
     </div>
