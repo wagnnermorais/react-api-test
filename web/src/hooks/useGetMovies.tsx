1784900bb3f4ae1fc9ef6233cc70bf1cdf85ff5d
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-15 18:24:24 -0300

    feat: creating provisional get request custom hook

---
+++ web/src/hooks/useGetMovies.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Movie {
+  _id: string;
+  title: string;
+}
+
+const useGetMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const url = "http://localhost:3000/api/movie";
+
+  const getMovies = async () => {
+    try {
+      const response = await axios.get<Movie[]>(url);
+      setMovies(response.data);
+    } catch (error) {
+      console.log(`Não foi possível realizar a operação!`);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return movies;
+};
+
+export default useGetMovies;
