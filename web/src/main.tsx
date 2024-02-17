7b3d0bedab5517619b1da5e0702dc977d8329a56
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-17 14:58:37 -0300

    feat: adding routing

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Layout from "./layout/Layout.tsx";
 import App from "./App.tsx";
+import RegisterMovie from "./pages/RegisterMovie.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/register" element={<RegisterMovie />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
