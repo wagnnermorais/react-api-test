b2753b835c6395cb2dc6ef8aba397b3ef1238417
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-20 22:05:59 -0300

    refac: changing input styling

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -16,12 +16,12 @@ const Input = ({
         placeholder={placeholder}
         onChange={onChange}
         value={value}
-        className="py-3 px-4 w-full peer rounded-md  text-sm text-gray-700 border-gray-200 border-2 focus:bg-white bg-white placeholder-shown:bg-gray-200 placeholder-shown:border-none placeholder-transparent"
+        className="py-3 px-4 w-full peer rounded text-base text-gray-700 border-2 border-black border-opacity-35 focus:bg-white bg-white placeholder-transparent outline-none"
         autoComplete="off"
       />
       <label
         htmlFor={name}
-        className="absolute left-2 -top-2.5 px-1 text-sm transition-all bg-white rounded text-gray-500 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-placeholder-shown:left-4 peer-placeholder-shown:bg-gray-200 peer-focus:-top-2.5 peer-focus:left-2 peer-focus:text-blue-500 peer-focus:bg-white"
+        className="absolute left-4 -top-3 px-2 text-base duration-200 bg-white rounded text-gray-500 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-placeholder-shown:left-4 peer-focus:-top-2.5 peer-focus:left-4 peer-focus:bg-white"
       >
         {title}
       </label>
