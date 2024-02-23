7186b510a0d22cd927d831adc5d05a9bc715f6a5
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-23 11:31:27 -0300

    style: adding floating label to form

--- web/src/pages/RegisterMovie.tsx
+++ web/src/pages/RegisterMovie.tsx
@@ -2,40 +2,40 @@ import Input from "../components/Input";
 
 const RegisterMovie = () => {
   return (
-    <form className="mt-12 mx-auto py-6 px-12 w-[50%]">
-      <div className="w-full my-6">
-        <Input title="teste" type="text" name="title" placeholder="Title" />
+    <form className="mt-12 mx-auto py-6 px-10 w-[50%] bg-white rounded">
+      <div className="my-6">
+        <Input title="Title" type="text" name="title" placeholder="Title" />
       </div>
-      <div className="w-full my-6">
-        <Input title="teste" type="text" name="rating" placeholder="Rating" />
+      <div className="my-6">
+        <Input title="Rating" type="text" name="rating" placeholder="Rating" />
       </div>
-      <div className="w-full my-6">
+      <div className="my-6">
         <Input
-          title="teste"
+          title="Description"
           type="text"
           name="description"
           placeholder="Description"
         />
       </div>
-      <div className="w-full my-6">
+      <div className="my-6">
         <Input
-          title="teste"
+          title="Director"
           type="text"
           name="director"
           placeholder="Director"
         />
       </div>
-      <div className="w-full my-6">
-        <Input title="teste" type="text" name="stars" placeholder="Stars" />
+      <div className="my-6">
+        <Input title="Stars" type="text" name="stars" placeholder="Stars" />
       </div>
-      <div className="w-full my-6">
-        <Input title="teste" type="text" name="poster" placeholder="Poster" />
+      <div className="my-6">
+        <Input title="Poster" type="text" name="poster" placeholder="Poster" />
       </div>
       <div className="flex items-center justify-center">
         <input
           type="submit"
           value="Register"
-          className="bg-white w-1/2 py-2 cursor-pointer"
+          className="py-3 w-1/3 text-md font-bold bg-zinc-950 text-white rounded cursor-pointer duration-300 hover:bg-opacity-90"
         />
       </div>
     </form>
