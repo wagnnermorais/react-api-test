e74f129c981355806ca16ee924b237c1253c9a2a
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-24 21:38:46 -0300

    feat: creating provisional post request custom hook

---
+++ web/src/hooks/useRegisterMovie.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { Movie } from "../types/Movie";
+import axios from "axios";
+
+const useRegisterMovie = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const url = "http://localhost:3000/api/movie";
+
+  const registerMovie = async (newMovie: Movie) => {
+    try {
+      const response = await axios.post<Movie>(url, newMovie);
+      console.log("Movie registered:", response.data);
+      setMovies([...movies, response.data]);
+    } catch (error) {
+      console.log("Failed to register movie:", error);
+    }
+  };
+
+  useEffect(() => {}, []);
+
+  return { movies, registerMovie };
+};
+
+export default useRegisterMovie;
