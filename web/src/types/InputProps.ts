560a1d469affba4473c99e1ca480c907066b486c
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-27 21:07:10 -0300

    refac: changing onChange type

--- web/src/types/InputProps.ts
+++ web/src/types/InputProps.ts
@@ -1,8 +1,9 @@
+import { ChangeEvent } from "react";
 export interface InputProps {
   title: string;
   type: string;
   name: string;
   placeholder?: string;
-  onChange?: () => void;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   value?: string | number;
 }
