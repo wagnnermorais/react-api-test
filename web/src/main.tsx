366341dbda2a2dd1a5204f1d8f05277bc729da3f
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-29 10:34:50 -0300

    feat: creating movies route

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import RegisterMovie from "./pages/RegisterMovie.tsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
+import MovieList from "./pages/MovieList.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route element={<Layout />}>
           <Route path="/" element={<App />} />
           <Route path="/register" element={<RegisterMovie />} />
+          <Route path="/movies" element={<MovieList />} />
         </Route>
       </Routes>
     </Router>
