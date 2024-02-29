cd8696b4299e6ca7c86e4efb43196e381cdba479
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-29 10:34:04 -0300

    refac: adding movie limiter

--- web/src/hooks/useGetMovies.tsx
+++ web/src/hooks/useGetMovies.tsx
@@ -2,22 +2,25 @@ import { useEffect, useState } from "react";
 import { Movie } from "../types/Movie";
 import axios from "axios";
 
-const useGetMovies = () => {
+const useGetMovies = (limit?: number) => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const url = "http://localhost:3000/api/movie";
 
-  const getMovies = async () => {
-    try {
-      const response = await axios.get<Movie[]>(url);
-      setMovies(response.data);
-    } catch (error) {
-      console.log(`Não foi possível realizar a operação!`);
-    }
-  };
-
   useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await axios.get<Movie[]>(url);
+        const renderLimit = limit
+          ? response.data.slice(0, limit)
+          : response.data;
+        setMovies(renderLimit);
+      } catch (error) {
+        console.log(`Não foi possível realizar a operação!`);
+      }
+    };
+
     getMovies();
-  }, []);
+  }, [url, limit]);
 
   return movies;
 };
