59617a8edf4e9a8118c9c571a30dbc7136810d85
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-29 10:48:22 -0300

    feat: adding modal to movie list component

--- web/src/pages/MovieList.tsx
+++ web/src/pages/MovieList.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { Movie } from "../types/Movie";
+import MovieModal from "../components/MovieModal";
 import useGetMovies from "../hooks/useGetMovies";
 
 const MovieList = () => {
   const movies: Movie[] = useGetMovies();
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSelectedMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
   return (
     <div className="grid grid-cols-4 gap-4 w-[80%] my-12 mx-auto">
       {movies.map((movie) => (
-        <div key={movie._id} className="border cursor-pointer">
+        <div
+          key={movie._id}
+          className="border cursor-pointer"
+          onClick={() => handleSelectedMovie(movie)}
+        >
           <img
             src={movie.poster}
             alt={movie.title}
@@ -14,6 +25,12 @@ const MovieList = () => {
           />
         </div>
       ))}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
     </div>
   );
 };
