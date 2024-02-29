feb05e4f837d6eccc92f8d72d467a472cd9e44c3
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-29 10:34:34 -0300

    feat: adding modal for each movie

--- web/src/components/Movies.tsx
+++ web/src/components/Movies.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+import { Movie } from "../types/Movie";
 import { MovieProps } from "../types/MovieProps";
+import MovieModal from "./MovieModal";
 import useGetMovies from "../hooks/useGetMovies";
 
 const Movies = ({ searchText }: MovieProps) => {
-  const movies = useGetMovies();
+  const movies: Movie[] = useGetMovies(12);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
   const filteredMovies = movies.filter((movie) =>
     movie.title.toLowerCase().includes(searchText.toLowerCase())
   );
+
+  const handleSelectedMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
   return (
     <div className="grid grid-cols-4 gap-4 w-[80%] my-12 mx-auto">
       {filteredMovies.map((movie) => (
-        <div key={movie._id} className="border">
+        <div
+          key={movie._id}
+          className="border cursor-pointer"
+          onClick={() => handleSelectedMovie(movie)}
+        >
           <img
             src={movie.poster}
             alt={movie.title}
@@ -17,6 +29,12 @@ const Movies = ({ searchText }: MovieProps) => {
           />
         </div>
       ))}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
     </div>
   );
 };
